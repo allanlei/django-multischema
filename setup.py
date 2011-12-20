780caaa7e55030456ae833f5e90db31684697572
Allan Lei <allanlei@helveticode.com>
2011-12-19 18:26:13 -0800

    update version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def find_packages_in(where, **kwargs):
 
 setup(
     name = 'django-multischema',
-    version = '0.1.2',
+    version = '0.1.3',
     author = 'Allan Lei',
     author_email = 'allanlei@helveticode.com',
     description = ('Multischema for Django'),
